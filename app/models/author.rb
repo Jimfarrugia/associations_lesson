0024a78fe9fc3165dbf7033fce1dc0a1fc894603
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-04-15 13:01:26 +1000

    created model and migration for Books

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ApplicationRecord
-  has_one :address
-  
+  has_one :address, dependent: :destroy
+  # has_one :address, class_name: "Address", foreign_key: "author_id"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_021528) do
+ActiveRecord::Schema.define(version: 2019_04_15_030058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,5 +33,14 @@ ActiveRecord::Schema.define(version: 2019_04_15_021528) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
   add_foreign_key "addresses", "authors"
+  add_foreign_key "books", "authors"
 end
